3b3945ac29ae24abcd9a10bc05705a61a6bb1fd6
suhyeon327 <jshh020327@gmail.com>
2026-06-04 23:17:18 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.05 ms, Memory: 65.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        
+        for (char ch : my_string.toCharArray()) {
+           if (Character.isDigit(ch) == true)
+               answer += ch - '0';
+        }
+        
+        return answer;
+    }
+}
