ae110513a474cfa1b4e514819e4bbf563895ade3
suhyeon327 <jshh020327@gmail.com>
2026-06-09 00:58:58 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 64.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+        if (n >= 10) {
+            answer = 12000 * n + 2000 * (k - (n / 10));
+        } else {
+            answer = 12000 * n + 2000 * k;
+        }
+        
+        return answer;
+    }
+}
